6b013458fba486ba18a055c0d457ec7b19b3e300
ZainAL-Ajjori <zainajjouri@gmail.com>
2026-02-15 20:45:45 +0200

    Add files via upload

---
+++ sahilha_db.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 15 فبراير 2026 الساعة 19:43
+-- إصدار الخادم: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sahilha_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `messages`
+--
+-- Error reading structure for table sahilha_db.messages: #1932 - Table &#039;sahilha_db.messages&#039; doesn&#039;t exist in engine
+-- Error reading data for table sahilha_db.messages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `sahilha_db`.`messages`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `services`
+--
+-- Error reading structure for table sahilha_db.services: #1932 - Table &#039;sahilha_db.services&#039; doesn&#039;t exist in engine
+-- Error reading data for table sahilha_db.services: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `sahilha_db`.`services`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `site_users`
+--
+-- Error reading structure for table sahilha_db.site_users: #1932 - Table &#039;sahilha_db.site_users&#039; doesn&#039;t exist in engine
+-- Error reading data for table sahilha_db.site_users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `sahilha_db`.`site_users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
